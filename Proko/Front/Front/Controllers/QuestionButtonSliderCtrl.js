7b2fad8b7517aa93341aa75d593dfd7250c475d5
Ilkka Alakarhu <ilkka_alakarhu@hotmail.com>
2016-11-20 15:35:43 +0200

    lisäätty AnswererTypeCtrl-tiedosto. Sitten laitettu kyselyn viewien eteneminen parissa kohtaa uuteen uskoon, eli muutettu 3:ssa viewissa sitä, minne buttonia painamalla seuraavaksi mennään.

--- Proko/Front/Front/Controllers/QuestionButtonSliderCtrl.js
+++ Proko/Front/Front/Controllers/QuestionButtonSliderCtrl.js
@@ -9,7 +9,7 @@ app.controller('QuestionButtonSliderCtrl', function QuestionButtonSliderCtrl($sc
                 answerService.setAnswer($scope.answerOne);
                 answerService.setAnswer($scope.answerTwo);
                 console.log("Tiedot talletettu!")
-                $location.path("/questionbuttons");
+                $location.path("/questiondragdrop");
             } else {
                 alert("Syötä vastaus 2!");
             }
