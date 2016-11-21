9fd9829bcf47280e2577f14182985d76748dc8a7
Ilkka Alakarhu <ilkka_alakarhu@hotmail.com>
2016-11-21 12:21:42 +0200

    Testausta questionDragdropctrl ja q-dragdrop tiedostoissa.

--- Proko/Front/Front/Controllers/QuestionDragDropCtrl.js
+++ Proko/Front/Front/Controllers/QuestionDragDropCtrl.js
@@ -1,5 +1,8 @@
 ﻿'use strict';
 app.controller('QuestionDragDropCtrl', function QuestionDragDropCtrl($scope, $location, answerService) {
+
+    $scope.list1 = { title: 'AngularJS - Drag Me' };
+    $scope.list2 = {};
     $scope.setAnswers = function () {
         $location.path("/end");
     }        
