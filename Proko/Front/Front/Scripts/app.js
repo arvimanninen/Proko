4fab78c4ceab8f9e09a7d95d5793c86ac885c0e2
arvimanninen <arvi.manninen@myy.haaga-helia.fi>
2016-11-24 20:33:46 +0200

    Bug fixes

--- Proko/Front/Front/Scripts/app.js
+++ Proko/Front/Front/Scripts/app.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-var app = angular.module('app', ["ngRoute", "chart.js", "ngDragDrop", angularDragula(angular)]);
+var app = angular.module('app', ["ngRoute", "chart.js", angularDragula(angular)]);
 
 
 app.config(['$routeProvider', '$locationProvider',
