60b75aea241c558bb4e91ccf84d4007deb582fb8
Ilkka Alakarhu <ilkka_alakarhu@hotmail.com>
2016-11-24 15:56:17 +0200

    muutettu vähän dragdrop smiley html:ää ja sen controlleria eli laitettu osa koodista kommenteiksi. Muita pieniä muutoksia, kun yrittänyt saada angular dragulan toimimaan.

--- Proko/Front/Front/Controllers/QuestionDragDropSmileysCtrl.js
+++ Proko/Front/Front/Controllers/QuestionDragDropSmileysCtrl.js
@@ -1,9 +1,8 @@
 ﻿app.controller('QuestionDragDropSmileysCtrl', function QuestionDragDropSmileysCtrl($scope, $location, answerService) {
 
-    $scope.list1 = { title: 'AngularJS - Drag Me' };
-    $scope.list2 = {};
 
-    $scope.smiley1 = { title: '' };
-    $scope.list2 = {};
+
+    //$scope.list1 = { title: 'AngularJS - Drag Me' };
+    //$scope.list2 = {};
 
 });
