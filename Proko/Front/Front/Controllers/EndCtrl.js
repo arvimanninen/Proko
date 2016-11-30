07e029a2d65f85a033742bd94284b1e232c7cc9e
arvimanninen <arvi.manninen@myy.haaga-helia.fi>
2016-11-30 21:22:27 +0200

    storageService integrated and few bugs fixed. Seems to work now.

--- Proko/Front/Front/Controllers/EndCtrl.js
+++ Proko/Front/Front/Controllers/EndCtrl.js
@@ -1,5 +1,6 @@
-﻿app.controller('EndCtrl', function EndCtrl($scope, answerService, storageService) {
-    $scope.allAnswersToStorage = function () {
-        storageService.setAnswers(answerService.getAllAnswers());
+﻿app.controller('EndCtrl', function EndCtrl($scope, $location, answerService, storageService) {
+    $scope.setNewAveragesAndGo = function () {
+        storageService.setAverages(answerService.getAllAnswers());
+        $location.path("/monitoringov");
     };
 });
