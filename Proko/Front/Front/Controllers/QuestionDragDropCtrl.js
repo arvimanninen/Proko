88ae3cae67e17927048fc441efddd008803ab297
Ilkka Alakarhu <ilkka_alakarhu@hotmail.com>
2016-12-06 19:31:44 +0200

    muutoksia.

--- Proko/Front/Front/Controllers/QuestionDragDropCtrl.js
+++ Proko/Front/Front/Controllers/QuestionDragDropCtrl.js
@@ -1,6 +1,13 @@
 ﻿'use strict';
 app.controller('QuestionDragDropCtrl', function QuestionDragDropCtrl($scope, $location, answerService, dragulaService) {
+    $scope.setAnswers = function () {
 
+        //var parent_1 = $("#dr-1").parent().attr("id").val();
+        //console.log(parent_1);
 
+        if (true) {
+            $location.path("/questiondragdropsmileys");
+        }
+    };
     
 });
