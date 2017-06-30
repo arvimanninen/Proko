642bf6b5fcdad985f600df5de9774145e43eb8d2
Ilkka Alakarhu <ilkka_alakarhu@hotmail.com>
2017-06-30 23:45:04 +0300

    commented some 2nd q ctrl code

--- Proko/Front/Front/Controllers/QuestionButtonSliderCtrl.js
+++ Proko/Front/Front/Controllers/QuestionButtonSliderCtrl.js
@@ -4,17 +4,19 @@ app.controller('QuestionButtonSliderCtrl', function QuestionButtonSliderCtrl($sc
     $scope.answerOne = "Not answered";
     $scope.answerTwo = "Not answered";
     $scope.setAnswers = function () {
-        if ($scope.answerOne === 1 || $scope.answerOne === 2 || $scope.answerOne === 3 || $scope.answerOne === 4 || $scope.answerOne === 5) {
-            if ($scope.answerTwo === 1 || $scope.answerTwo === 2 || $scope.answerTwo === 3 || $scope.answerTwo === 4 || $scope.answerTwo === 5) {
-                answerService.setAnswer($scope.answerOne);
-                answerService.setAnswer($scope.answerTwo);
-                console.log("Tiedot talletettu!");
-                $location.path("/questionsafety");
-            } else {
-                alert("Syötä vastaus 2!");
-            }
-        } else {
-            alert("Syötä vastaus 1!");
-        }
+
+        $location.path("/questionsafety");
+        //if ($scope.answerOne === 1 || $scope.answerOne === 2 || $scope.answerOne === 3 || $scope.answerOne === 4 || $scope.answerOne === 5) {
+        //    if ($scope.answerTwo === 1 || $scope.answerTwo === 2 || $scope.answerTwo === 3 || $scope.answerTwo === 4 || $scope.answerTwo === 5) {
+        //        answerService.setAnswer($scope.answerOne);
+        //        answerService.setAnswer($scope.answerTwo);
+        //        console.log("Tiedot talletettu!");
+        //        $location.path("/questionsafety");
+        //    } else {
+        //        alert("Syötä vastaus 2!");
+        //    }
+        //} else {
+        //    alert("Syötä vastaus 1!");
+        //}
     };
 });
