ba348996e09b71637d48aa8d3011eaa097af6fbf
arvimanninen <arvi.manninen@myy.haaga-helia.fi>
2017-08-24 18:55:56 +0300

    Start controller and Start template updated with some minor changes done

--- Proko/Back/Back/surveyapp/app/start/start.controller.js
+++ Proko/Back/Back/surveyapp/app/start/start.controller.js
@@ -3,8 +3,11 @@ app.controller('StartCtrl', function StartCtrl($location, DataFactory, QuestionS
     console.log("StartCtrl started");
     var that = this;
     var startButton = document.getElementById("start-button");
+    // NAMED lt INSTEAD OF loadingText FOR AVOIDING CONFUSION WITH that.loadingText
+    var lt = document.getElementById("loading-text");
+
     startButton.style.visibility = "hidden";
-    that.loadText = "Kysymyksiä ladataan...";
+    that.loadingText = "Kysymyksiä ladataan...";
        
     var rawQs = DataFactory.getChosenQuestions.query(function () {
         console.log("All get!");
@@ -15,14 +18,17 @@ app.controller('StartCtrl', function StartCtrl($location, DataFactory, QuestionS
             console.log("rawQs[" + i + "].Text: " + rawQs[i].Text);
             console.log("rawQs[" + i + "].QuestionMethodValue: " + rawQs[i].QuestionMethodValue);
         }
-        if (rawQs.length === 0) {
-            that.loadText = "Kysymyksiä ei löydy! Sulje selain ja yritä uudestaan";
-        }
-        QuestionService.setQuestions(rawQs);
-        that.loadText = "Kysymykset ladattu!";
         $("#loading-icon").remove();
-        startButton.style.visibility = "visible";
-        console.log("QuestionService.getQuestion(0).QuestionMethodValue" + QuestionService.getQuestion(0).QuestionMethodValue);
+        if (rawQs.length === 0) {
+            that.loadingText = "Kysymyksiä ei löydy! Sulje selain ja yritä uudestaan";
+            lt.style.color = "red";
+        } else {
+            QuestionService.setQuestions(rawQs);
+            that.loadingText = "Kysymykset ladattu!";
+            lt.style.color = "green";
+            startButton.style.visibility = "visible";
+            console.log("QuestionService.getQuestion(0).QuestionMethodValue" + QuestionService.getQuestion(0).QuestionMethodValue);
+        }
     });
 
     that.startSurvey = function () {
