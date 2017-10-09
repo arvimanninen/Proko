6fe98af774d1b033832fe7f8620901e8cc5e14cf
Ilkka Alakarhu <ilkka_alakarhu@hotmail.com>
2017-10-09 12:06:46 +0300

    Update monitoringov view styles

--- Proko/Front/Front/Controllers/MonitoringOvCtrl.js
+++ Proko/Front/Front/Controllers/MonitoringOvCtrl.js
@@ -5,16 +5,45 @@
          console.log(" MonitoringOvCtrl.answerAverages[" + i + "]: " + answerAverages[i]);
      }
      $scope.answerCount = count;
-     $scope.pollData = answerAverages;
-     $scope.pollLabels = ['Kysymys 1', 'Kysymys 2', 'Kysymys 3'];
-
-     $scope.pollSeries = ['Tyytyvaisyys'];
-     $scope.options = {
+     $scope.pollData1 = answerAverages;
+     $scope.pollLabels1 = ['Kysymys 1', 'Kysymys 2', 'Kysymys 3'];
+     $scope.pollSeries1 = ['Tyytyvaisyys'];
+     $scope.options1 = {
          scales: {
              yAxes: [{
                  ticks: {
                      max: 5,
-                     min: 0.8,
+                     min: 0,
+                     stepSize: 1
+                 }
+             }]
+         }
+     };
+
+     $scope.pollData2 = answerAverages;
+     $scope.pollLabels2 = ['Kysymys 1', 'Kysymys 2', 'Kysymys 3'];
+     $scope.pollSeries2 = ['Tyytyvaisyys'];
+     $scope.options2 = {
+         scales: {
+             yAxes: [{
+                 ticks: {
+                     max: 5,
+                     min: 0,
+                     stepSize: 1
+                 }
+             }]
+         }
+     };
+
+     $scope.pollData3 = answerAverages;
+     $scope.pollLabels3 = ['Kysymys 1', 'Kysymys 2', 'Kysymys 3'];
+     $scope.pollSeries3 = ['Tyytyvaisyys'];
+     $scope.options3 = {
+         scales: {
+             yAxes: [{
+                 ticks: {
+                     max: 5,
+                     min: 0,
                      stepSize: 1
                  }
              }]
