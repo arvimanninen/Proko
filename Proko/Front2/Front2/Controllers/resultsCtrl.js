559e9162fec21ef453aeaf6bd80fb77a76f0a7ef
Ilkka Alakarhu <ilkka_alakarhu@hotmail.com>
2017-10-16 15:53:07 +0300

    Move popup to results view, add disabled functionality to it

--- Proko/Front2/Front2/Controllers/resultsCtrl.js
+++ Proko/Front2/Front2/Controllers/resultsCtrl.js
@@ -2,7 +2,56 @@
 
 app.controller('resultsCtrl', function resultsCtrl($scope, $location) {
 
-    $scope.previousView = function () {
+    //modal
+    $(function () {
+        var counterUsed = false;
+
+        var appendthis = ("<div class='modal-overlay js-modal-close'></div>");
+        $('a[data-modal-id]').click(function (e) {
+
+            if (counterUsed === false) {
+
+                e.preventDefault();
+                $("body").append(appendthis);
+                $(".modal-overlay").fadeTo(500, 0.7);
+                //$(".js-modalbox").fadeIn(500);
+                var modalBox = $(this).attr('data-modal-id');
+                $('#' + modalBox).fadeIn($(this).data());
+            }
+        });
+
+        $(".js-modal-close, .modal-overlay").click(function () {
+            $(".modal-box, .modal-overlay").fadeOut(500, function () {
+                $(".modal-overlay").remove();
+            });
+        });
+
+        $(".js-modal-use, .modal-overlay").click(function () {
+            $(".modal-box, .modal-overlay").fadeOut(500, function () {
+                $(".modal-overlay").remove();
+                $(".popup-div a").html("Kuponki lunastettu")
+                    .removeClass("btn-success").addClass("disabled");
+
+                counterUsed = true;
+                console.log(counterUsed);
+
+                $("#popup").remove();
+            });
+        });
+
+        $(window).resize(function () {
+            $(".modal-box").css({
+                top: ($(window).height() - $(".modal-box").outerHeight()) / 2,
+                left: ($(window).width() - $(".modal-box").outerWidth()) / 2
+            });
+        });
+
+        $(window).resize();
+
+    }); //modal ends
+
+
+    $scope.toStart = function () {
         $location.path("/end");
     };
 
