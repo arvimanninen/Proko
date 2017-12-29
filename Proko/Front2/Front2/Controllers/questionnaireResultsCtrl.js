2b9e405ac85eaae8e57d39868cd5cd1e2f267700
Ilkka Alakarhu <ilkka_alakarhu@hotmail.com>
2017-12-29 19:38:53 +0200

    Update qu-naire ctrl chart 3 code.

--- Proko/Front2/Front2/Controllers/questionnaireResultsCtrl.js
+++ Proko/Front2/Front2/Controllers/questionnaireResultsCtrl.js
@@ -8,13 +8,10 @@ app.controller('questionnaireResultsCtrl', function questionnaireResultsCtrl($sc
 
     $scope.answerCount = 20;
 
-    //$("#canvas1").parentNode.style.height = '400px';
-
     $scope.chart1 = {
         labels: ['20.11.', '27.11.', '4.12.', '11.12.', '18.12.'],
-        series: ['Rakennusmies', 'Sähkömies', 'LVI asentaja', 'Putkimies'],
-        data: [[3.4, 3.9, 3.7, 4.5, 4.0], [2.2, 3.0, 3.4, 3.1, 3.7], [3.1, 3.2, 3.0, 3.7, 4.3], [2.5, 2.4, 3.0, 2.8, 3.2]],
-        colors: ['#8ed791', '#d7d78e', '#d78e8e', '#c58ed7', '#8eb3d7'],
+        data: [[2.4, 3.2, 3.7, 3.3, 4.0]],
+        colors: ['#8ed791'],
         options: {
             scales: {
                 yAxes: [
@@ -48,6 +45,77 @@ app.controller('questionnaireResultsCtrl', function questionnaireResultsCtrl($sc
             legend: {
                 display: false
             },
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-1',
+                        display: true,
+                        type: 'linear',
+                        position: 'left',
+                        ticks: {
+                            display: false,
+                            min: 0,
+                            max: 5,
+                            beginAtZero: true,
+                            stepSize: 1.25
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Tyytyväisyys"
+                        }
+                    }
+                ]
+            }
+        }
+    };
+
+    $scope.chart3 = {
+        labels: ['Vaihtoehto 1', 'Vaihtoehto 2', 'Vaihtoehto 3', 'Vaihtoehto 4'],
+        data: [3, 9, 7, 1],
+        colors: ['rgba(142,215,145,0.35)', 'rgba(142,215,145,0.35)', 'rgba(142,215,145,0.35)', 'rgba(142,215,145,0.35)', 'rgba(142,215,145,0.35)'],
+        options: {
+            scales: {
+                yAxes: [{
+                    gridLines: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        min: 0,
+                        mirror: true,
+                        fontSize: 18,
+                        padding: -10
+                    }
+                }],
+                xAxes: [{
+                    gridLines: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        display: false,
+                        min: 0
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            },
+            title: {
+                display: false,
+            },
+            barShowStroke: false,
+            barStrokeWidth: 10,
+            barValueSpacing: 0
+        }
+    };
+
+    $scope.chartxx = {
+        labels: ['20.11.', '27.11.', '4.12.', '11.12.', '18.12.'],
+        series: ['Rakennusmies', 'Sähkömies', 'LVI asentaja', 'Putkimies'],
+        data: [[3.4, 3.9, 3.7, 4.5, 4.0], [2.2, 3.0, 3.4, 3.1, 3.7], [3.1, 3.2, 3.0, 3.7, 4.3], [2.5, 2.4, 3.0, 2.8, 3.2]],
+        colors: ['#8ed791', '#d7d78e', '#d78e8e', '#c58ed7', '#8eb3d7'],
+        options: {
             scales: {
                 yAxes: [
                     {
--- Proko/Front2/Front2/Scripts/app.js
+++ Proko/Front2/Front2/Scripts/app.js
@@ -70,7 +70,7 @@ app.config(['$routeProvider', '$locationProvider', 'ChartJsProvider',
 
             ChartJsProvider.setOptions({
                 global: {
-                    //colors: ['#8ed791', '#d7d78e', '#d78e8e', '#c58ed7', '#8eb3d7'],
+                    colors: ['#8ed791', '#d7d78e', '#d78e8e', '#c58ed7', '#8eb3d7'],
                     defaultFontColor: '#333333',
                     defaultFontFamily: 'Roboto',
                     defaultFontSize: 14,
@@ -80,7 +80,7 @@ app.config(['$routeProvider', '$locationProvider', 'ChartJsProvider',
                         }
                     },
                     legend: {
-                        display: true,
+                        display: false,
                         position: 'bottom'
                     },
                     tooltips: {
