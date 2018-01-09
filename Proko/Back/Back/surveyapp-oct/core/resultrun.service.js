05be094c23ae38a81a8ef51f119393452776c568
arvimanninen <arvi.manninen@myy.haaga-helia.fi>
2018-01-09 19:28:48 +0200

    ResultRunService created

---
+++ Proko/Back/Back/surveyapp-oct/core/resultrun.service.js
@@ -0,0 +1,25 @@
+﻿' use strict';
+app.service('ResultRunService', function () {
+    var resultCount = 0;
+    var currentResultCount = 0;
+
+    var getResultCount = function () {
+        return resultCount;
+    };
+    var setResultCount = function (nResultCount) {
+        resultCount = nResultCount;
+    };
+    var getCurrentResultCount = function () {
+        return currentResultCount;
+    };
+    var setCurrentResultCount = function (nCurrentResultCount) {
+        currentResultCount = nCurrentResultCount;
+    };
+
+    return {
+        getResultCount: getResultCount,
+        setResultCount: setResultCount,
+        getCurrentResultCount: getCurrentResultCount,
+        setCurrentResultCount: setCurrentResultCount
+    };
+});
