e32d01db8a9532ff8ced6cf3f58c747a1e23a46f
arvimanninen <arvi.manninen@myy.haaga-helia.fi>
2018-02-11 17:39:06 +0200

    Some code added to QuestionCtrl, EndCtrl and TextAreaCtrl

--- Proko/Back/Back/surveyapp-oct/end/end.controller.js
+++ Proko/Back/Back/surveyapp-oct/end/end.controller.js
@@ -3,6 +3,19 @@
 app.controller('EndCtrl', function ($location, AnswerService, AnswerBundleExtrasService,
     AnswersAndBundleExtrasService, DataFactory, ResultService) {
     var that = this;
+    $(document).ready(function () {
+        var wrapperHeight = $("#wrapper").height();
+        var containerHeight = $(".container").height();
+        var navbtnHeight = wrapperHeight - containerHeight;
+        if ((navbtnHeight) < 64) {
+            navbtnHeight = 52
+        } else {
+            navbtnHeight = navbtnHeight - 12;
+        }
+        console.log(navbtnHeight);
+        $(".nav-btn-div").css("margin-top", navbtnHeight);
+    });
+
     var postingSuccess = document.getElementById("posting-success");
     var postingIcon = document.getElementById("posting-icon");
     postingSuccess.style.visibility = "hidden";
--- Proko/Back/Back/surveyapp-oct/text-area/text-area.controller.js
+++ Proko/Back/Back/surveyapp-oct/text-area/text-area.controller.js
@@ -1,5 +1,17 @@
 ﻿app.controller('TextAreaCtrl', function ($location, QuestionService, AnswerBundleExtrasService, RunService) {
     var that = this;
+    $(document).ready(function () {
+        var wrapperHeight = $("#wrapper").height();
+        var containerHeight = $(".container").height();
+        var navbtnHeight = wrapperHeight - containerHeight;
+        if ((navbtnHeight) < 64) {
+            navbtnHeight = 52
+        } else {
+            navbtnHeight = navbtnHeight - 42;
+        }
+        console.log(navbtnHeight);
+        $(".nav-btn-div").css("margin-top", navbtnHeight);
+    });
     var textFb = document.getElementById("text-fb");
     textFb.value = AnswerBundleExtrasService.getTextFeedback();
     
