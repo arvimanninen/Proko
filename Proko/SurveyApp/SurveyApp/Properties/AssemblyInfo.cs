d1cc97bfda5b7fcceb12d373fc41f9b69df9952f
arvimanninen <arvi.manninen@zoho.eu>
2018-08-07 18:22:47 +0300

    Back folder renamed as SurveyApp, Front and Front2 folders renamed as StaticFront and StaticFront2. Paths changed, everything seems to work.

--- Proko/SurveyApp/SurveyApp/Properties/AssemblyInfo.cs
+++ Proko/SurveyApp/SurveyApp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Back")]
+[assembly: AssemblyTitle("Proko Survey App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Back")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Proko Survey App")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
