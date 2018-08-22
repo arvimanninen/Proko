afae3769c6b7cfdcecfd68582eba06f75afd9b01
arvimanninen <arvi.manninen@zoho.eu>
2018-08-22 18:07:04 +0300

    Comments added for progressBar -component

--- Proko/SurveyApp/SurveyApp/clientapp/progress-bar/progress-bar.component.js
+++ Proko/SurveyApp/SurveyApp/clientapp/progress-bar/progress-bar.component.js
@@ -1,8 +1,7 @@
 ﻿'use strict';
-// app-SOVELLUSMODUULIIN LUODAAN KOMPONENTTI progressBar
+// progressBar
+// - Component
 app.component('progressBar', {
-    // KOMPONENTIN KÄYTTÄMÄN NÄKYMÄPOHJAN URL
     templateUrl: 'progress-bar/progress-bar.template.html',
-    // KOMPONENTIN KÄYTTÄMÄN KONTROLLERIN NIMI
     controller: 'ProgressBarCtrl'
 });
--- Proko/SurveyApp/SurveyApp/clientapp/progress-bar/progress-bar.controller.js
+++ Proko/SurveyApp/SurveyApp/clientapp/progress-bar/progress-bar.controller.js
@@ -1,17 +1,26 @@
 ﻿'use strict';
+// ProgressBarCtrl
+// - Controller for progressBar -component
+
 app.controller('ProgressBarCtrl', function (RunService) {
     var that = this;
     var prBar = document.getElementById("pr-bar");
     that.progress = 0;
-
+ 
     var currentStep = RunService.getQuestionSetIndex() - 1;
     var maxSteps = RunService.getQuestionSetCount();
     var progressPerStep = 100 / maxSteps;
+    // Current progress is calculated with
     var currentProgress = currentStep * progressPerStep;
     console.log("progressBar.currentProgress: " + currentProgress);
 
+    // Checks that currentProgress is between 0 and 100
+    // If not, error message is shown in console and progress bar element (prBar)
+    // remains empty (value 0)
     if (currentProgress <= 100 && currentProgress >= 0) {
+        // Template bindable progress for WAI-ARIA
         that.progress = currentProgress;
+        // Progress bar is filled by current progress (currentProgress) in percents
         prBar.style.width = currentProgress + "%";
     } else {
         alert("Virhe etenemispalkissa!");
