4254ab2fc6dd9632d31fb64bd495fcd5806e2205
arvimanninen <arvi.manninen@zoho.eu>
2018-08-24 15:32:08 +0300

    Comments added to AnswersAndBundleExtrasService

--- Proko/SurveyApp/SurveyApp/clientapp/core/answersandbundleextras.service.js
+++ Proko/SurveyApp/SurveyApp/clientapp/core/answersandbundleextras.service.js
@@ -1,7 +1,11 @@
 ﻿'use strict';
+// AnswersAndBundleExtrasService
+// - Client-side data model for answers and bundle extras (answerer type, written feedback)
+// - Used for sending data to REST API in correct format
 app.service('AnswersAndBundleExtrasService', function () {
     var answersAndBundleExtras = null;
 
+    // Object constructor
     var AnswersAndBundleExtrasDTO = function (nAnswerDtos, nExtrasDto) {
         this.AnswerDtos = [];
         this.AnswerBundleExtrasDto = nExtrasDto;
@@ -9,12 +13,27 @@ app.service('AnswersAndBundleExtrasService', function () {
             this.AnswerDtos.push(nAnswerDtos[i]);
         }
     };
+
+    // getAnswersAndBundleExtras()
+    // - Returns AnswersAndBundleExtrasService.answersAndBundleExtras -object
+    // @return {AnswersAndBundleExtrasDTO} answersAndBundleExtras
     var getAnswersAndBundleExtras = function () {
         return answersAndBundleExtras;
     };
+
+    // setAnswersAndBundleExtras()
+    // - Gets answers (answerDtos) and bundle extras (extrasDto) as parameters
+    // - Creates new AnswersAndBundleExtrasDTO with answerDtos and extrasDto
+    //   as constructor parameters, and sets created AnswersAndBundleExtrasDTO object
+    //   to AnswersAndBundleExtrasService.answersAndBundleExtras
+    // @param {Array<AnswerDTO> answerDtos}
+    // @param {AnswerBundleExtrasDto extrasDto}
     var setAnswersAndBundleExtras = function (answerDtos, extrasDto) {
         answersAndBundleExtras = new AnswersAndBundleExtrasDTO(answerDtos, extrasDto);
     };
+
+    // reset()
+    // - Function will set AnswersAndBundleExtrasService to its default state
     var reset = function () {
         answersAndBundleExtras = null;
     };
