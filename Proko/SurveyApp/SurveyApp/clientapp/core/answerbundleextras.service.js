b3a628b9412d2f0799ca57d7f48f8da4d66cc725
arvimanninen <arvi.manninen@zoho.eu>
2018-08-24 15:21:16 +0300

    Comments added to AnswerBundleExtrasService

--- Proko/SurveyApp/SurveyApp/clientapp/core/answerbundleextras.service.js
+++ Proko/SurveyApp/SurveyApp/clientapp/core/answerbundleextras.service.js
@@ -1,8 +1,10 @@
 ﻿'use strict';
+// AnswerBundleExtrasService
+// - Local data model for written feedback (textFeedback) and answerer type (answererTypeId)
 app.service('AnswerBundleExtrasService', function () {
     var textFeedback = "";
     // TODO: CHANGE THIS!!!!
-    var answererTypeId = 1;
+    var answererTypeId = -1;
     /*
     public class AnswerBundleExtrasDTO
     {
@@ -10,28 +12,56 @@ app.service('AnswerBundleExtrasService', function () {
         public string TextFeedback { get; set; }
     }
     */
+    // Object constructor for exporting data from AnswerBundleExtrasService.
     var AnswerBundleExtrasDTO = function (nAnswererTypeId, nTextFeedback) {
         this.AnswererTypeID = nAnswererTypeId;
         this.TextFeedback = nTextFeedback;
     };
 
+    // getTextFeedback()
+    // - Function returns written feedback (AnswerBundleExtrasService.textFeedback).
+    // @return {String} textFeedback
     var getTextFeedback = function () {
         return textFeedback;
     };
+
+    // setTextFeedback()
+    // - Function gets written feedback as parameter, convert it as a string
+    //   and sets converted value to AnswerBundleExtrasService.textFeedback -variable.
+    // @param {=} feedback 
     var setTextFeedback = function (feedback) {
         textFeedback = feedback.toString();
     };
+
+    // getAnswererTypeId()
+    // - Function returns AnswerBundleExtrasService.answererTypeId -variable.
+    // @return {Number} answererTypeId
     var getAnswererTypeId = function () {
         return answererTypeId;
     };
+
+    // setAnswererTypeId()
+    // - Function gets new answerer type id as a parameter and
+    //   sets it value to AnswerBundleExtrasService.answererTypeId -variable.
+    // @param {Number} atId
     var setAnswererTypeId = function (atId) {
         answererTypeId = atId;
         console.log("AnswerBundleExtrasService.answererTypeId changed!. New answererTypeId: " + answererTypeId);
     };
+
+    // getAnswerBundleExtras()
+    // - Function creates new AnswerBundleExtrasDTO object (abe) using
+    //   AnswerBundleExtrasService.answererTypeId and
+    //   AnswerBundleExtrasService.textFeedback -variables as parameters,
+    //   and returns abe.
+    // @return {AnswerBundleExtrasDTO} abe
     var getAnswerBundleExtras = function () {
         var abe = new AnswerBundleExtrasDTO(answererTypeId, textFeedback);
         return abe;
     };
+
+    // reset()
+    // - Function will set AnswerBundleExtrasService to its default state
     var reset = function () {
         textFeedback = "";
         answererTypeId = -1;
