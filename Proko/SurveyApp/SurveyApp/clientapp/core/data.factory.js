a49dfd665466aec29062ab21b98226cfcc2d0ea0
arvimanninen <arvi.manninen@zoho.eu>
2018-08-26 14:13:49 +0300

    Comments added to DataFactory, minor modifications in index.html

--- Proko/SurveyApp/SurveyApp/clientapp/core/data.factory.js
+++ Proko/SurveyApp/SurveyApp/clientapp/core/data.factory.js
@@ -1,36 +1,27 @@
 ﻿'use strict';
-
+// DataFactory
+// - For interaction with REST API
 app.factory('DataFactory', function ($resource) {
-    console.log("dataFactory started!");
 
     return {
+        // get questions currently chosen in server-side (HTTP GET)
+        // {Array<QuestionDTO>}
         getChosenQuestions: $resource('../api/getchosenquestions'),
+
+        // get results to currently chosen questions (HTTP GET)
+        // {Array<AnswerResultDTO>}
         getResultsToChosenQuestions: $resource('../api/getresultstochosenquestions'),
+
+        // get answerer types currently chosen in server side (HTTP GET)
+        // {Array<AnswererTypeDTO>}
         getChosenAnswererTypes: $resource('../api/getchosenanswerertypes'),
+
+        // post answers and bundle extras to server-side (HTTP POST)
+        // {AnswersAndBundleExtrasDTO}
         postSurveyAnswers: $resource('../api/postsurveyanswers')
     };
 
 });
-
-/*
-var getChosenQuestions = function() {
-    console.log("dataService.getChosenQuestion() started");
-    var chosen = ChosenQuestions.query();
-    console.log("cg.length: " + chosen.length)
-    for (var i = 0; i < chosen.length; i++) {
-        console.log("*#*#*#*#*#");
-        console.log("chosen[" + i + "].QuestionID: " + chosen[i].QuestionID);
-        console.log("chosen[" + i + "].ChosenIndex: " + chosen[i].ChosenIndex);
-        console.log("chosen[" + i + "].Text: " + chosen[i].Text);
-        console.log("chosen[" + i + "].QuestionMethodValue: " + chosen[i].QuestionMethodValue);
-    }
-
-    if(cq.length > 0) {
-        return ChosenQuestions;
-    } else {
-        console.log("Fail in get");
-        alert("Fail in get!");
-    }*/
 
 
 
