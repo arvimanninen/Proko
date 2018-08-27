2ed4dc4aaa45da0b65072e6bdfa74b00f7e4da6e
arvimanninen <arvi.manninen@zoho.eu>
2018-08-27 15:46:54 +0300

    Comments added to QuestionCtrl

--- Proko/SurveyApp/SurveyApp/clientapp/core/question.controller.js
+++ Proko/SurveyApp/SurveyApp/clientapp/core/question.controller.js
@@ -1,8 +1,11 @@
 ﻿'use strict';
-
+// QuestionCtrl
+// - Controller used by qmButtonsSmileys, qmButtonsSlider and qmButtonThumbs -components
+// TODO: RENAME AS QuestionMethodCtrl OR QmCtrl
 app.controller('QuestionCtrl', function ($location, $route, $templateCache, AnswerService, QuestionService, RunService) {
     var that = this;
-    
+
+    // Template adjustments
     $(document).ready(function () {
         var wrapperHeight = $("#wrapper").height();
         var containerHeight = $(".container").height();
@@ -16,13 +19,27 @@ app.controller('QuestionCtrl', function ($location, $route, $templateCache, Answ
         $(".nav-btn-div").css("margin-top", navbtnHeight);
     });
 
+    // Checks that buttons in app are used for navigation
     if (RunService.getRouteButtonsUsed() === false) {
         $location.path("/error");
     }
     RunService.setRouteButtonsUsed(false);
+    // Gets current questionSetIndex
     var questionSetIndex = RunService.getQuestionSetIndex();
     console.log("questionSetIndex: " + questionSetIndex);
+    // Gets question based on questionSetIndex
     that.currentQuestions = QuestionService.getQuestionsBySetIndex(questionSetIndex);
+    // that.setAnswer()
+    // - Function is executed when user clicks answering button or other answer method in UI.
+    // - Function gets answerIndex based on question set index (questionSetIndex) and 
+    //   chosen question index (chosenQuestionIndex). If question already has answer (answerIndex >= 0), answer is replaced.
+    //   If question doesn't have answer (answerIndex === 1), answer is set. Other answerIndex values tell
+    //   that there is a bug in application, and error message is shown.
+    // @param {Number} value
+    // @param {Number} questionSetIndex
+    // @param {Number} chosenQuestionIndex
+    // @param {Number} questionId
+    // @param {Number} questionMethodId
     that.setAnswer = function (value, questionSetIndex, chosenQuestionIndex, questionId, questionMethodId) {
         // Check if Answer exists
         var answerIndex = AnswerService.getAnswerIndex(questionSetIndex, chosenQuestionIndex);
@@ -36,7 +53,7 @@ app.controller('QuestionCtrl', function ($location, $route, $templateCache, Answ
         } else {
             alert("Tuntematon virhe @ QuestionCtrl ");
         }
- 
+        /*
         var allAnswers = AnswerService.getAnswers();
         console.log("");
         console.log("");
@@ -46,10 +63,8 @@ app.controller('QuestionCtrl', function ($location, $route, $templateCache, Answ
             console.log("allAnswers.[" + i + "].QuestionMethodID" + allAnswers[i].QuestionMethodID);
             console.log("**************");
         }
-
-
+        */
     };
-
 });
 
 
