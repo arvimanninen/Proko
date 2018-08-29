7596376b63b1e05ae5f93779f505827827d0e91a
arvimanninen <arvi.manninen@zoho.eu>
2018-08-29 15:04:39 +0300

    Comments added to ResultChartLineSingleCtrl

--- Proko/SurveyApp/SurveyApp/clientapp/result-chart-line-single/result-chart-line-single.controller.js
+++ Proko/SurveyApp/SurveyApp/clientapp/result-chart-line-single/result-chart-line-single.controller.js
@@ -3,14 +3,17 @@
 // - Controller for resultChartLineSingle -component
 app.controller('ResultChartLineSingleCtrl', function ($attrs, QuestionService, AnswererTypeService, ResultService) {
     var that = this;
+    // - Attribute from HTML template
     var chartIndex = $attrs.questionindex;
-    
     var question = QuestionService.getQuestion(chartIndex);
     var questionId = question.QuestionID;
     that.questionText = question.QuestionText;
+    // - Gets five week averages based on question.QuestionID for chart
     var questionAverages = ResultService.getAveragesForAll(question.QuestionID);
+    // - Gets dates for constructing date strings for chart
     var dps = ResultService.getDatePoints();
 
+    // - Checks that questionAverages[] and dps[] have correct length
     if (questionAverages.length !== 5) {
         console.log("Invalid questionAverages.length @ ResultChartlineSingleCtrl!")
         alert("Invalid questionAverages.length @ ResultChartlineSingleCtrl!");
@@ -19,7 +22,12 @@ app.controller('ResultChartLineSingleCtrl', function ($attrs, QuestionService, A
         console.log("Invalid dps.length @ ResultChartLineSingleCtrl!");
         alert("Invalid dps.length @ ResultChartLineSingleCtrl!");
     }
-    
+
+    // getDayAndMonthString()
+    // - Function gets Date object (dateObject) as a parameter, 
+    //   and returns date string in following format: DD.MM.
+    // @param {Date} dateObject
+    // @return {String} str
     var getDayAndMonthString = function (dateObject) {
         var day = dateObject.getDate();
         var month = dateObject.getMonth() + 1;
@@ -31,6 +39,7 @@ app.controller('ResultChartLineSingleCtrl', function ($attrs, QuestionService, A
         console.log("questionAverages[" + i + "]: " + questionAverages[i]);
     }
 
+    // Chart options for Angular-Chart.js/Chart.js chart
     that.chart1 = {
         labels: [getDayAndMonthString(dps[4]), getDayAndMonthString(dps[3]), getDayAndMonthString(dps[2]), getDayAndMonthString(dps[1]), getDayAndMonthString(dps[0])],
         data: [ [questionAverages[4], questionAverages[3], questionAverages[2], questionAverages[1], questionAverages[0]] ],
