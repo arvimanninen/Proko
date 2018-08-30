429dbd21bf07de075f8b55867e053f3827453d66
arvimanninen <arvi.manninen@zoho.eu>
2018-08-30 16:07:32 +0300

    Dependencies (components and directives) added to start -component comments. textArea -component comments extended.

--- Proko/SurveyApp/SurveyApp/clientapp/start/start.component.js
+++ Proko/SurveyApp/SurveyApp/clientapp/start/start.component.js
@@ -2,6 +2,7 @@
 // start
 // - Component which is the first view component when app is launched.
 //   View component.
+// - Dependencies (components and directives): dynamicChartRow.
 app.component('start', {
     templateUrl: 'start/start.template.html',
     controller: 'StartCtrl'
--- Proko/SurveyApp/SurveyApp/clientapp/text-area/text-area.component.js
+++ Proko/SurveyApp/SurveyApp/clientapp/text-area/text-area.component.js
@@ -1,6 +1,8 @@
 ﻿'use strict';
-// textArea
-// - Component
+// start
+// - Component which is the used for giving optional written feedback.
+//   View component.
+// - Dependencies (components and directives): progressBar.
 app.component('textArea', {
     templateUrl: 'text-area/text-area.template.html',
     controller: 'TextAreaCtrl'
